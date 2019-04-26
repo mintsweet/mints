efab57df6a6a366a5e33f7f6394ea69058ccc747
mintsweet <0x1304570@gmail.com>
2019-04-26 10:33:46 +0800

    add global document in .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,15 @@ const eslintConfig = {
   parser: 'babel-eslint',
 
   globals: {
-    window: true
+    window: true,
+    document: true,
   },
 
   extends: './packages/eslint-config-mints/index.js',
 
   rules: {
     'no-console': 0,
-    'global-require': 0
+    'global-require': 0,
   }
 };
 
