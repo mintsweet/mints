0e55be985c1da772b11dd951f899a92797804fa8
mintsweet <0x1304570@gmail.com>
2019-07-03 10:26:09 +0800

    mints-utils: fix import path problem

--- packages/mints-utils/index.js
+++ packages/mints-utils/index.js
@@ -1,7 +1,3 @@
-export { default as $ } from './src/dom';
-export { default as env } from './src/env';
-export { default as format } from './src/format';
-export { default as storage } from './src/storage';
-export { default as tool } from './src/tool';
-export { default as url } from './src/url';
-export { default as Http } from './src/Http';
+const lib = require('./src');
+
+module.exports = lib;
---
+++ packages/mints-utils/src/index.js
@@ -0,0 +1,7 @@
+export { default as $ } from './dom';
+export { default as env } from './env';
+export { default as format } from './format';
+export { default as storage } from './storage';
+export { default as tool } from './tool';
+export { default as url } from './url';
+export { default as Http } from './Http';
